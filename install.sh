a1a51f062e03a71962b49a1c3e7ff4213bb99c8f
Jiri Marsicek <jiri.marsicek@gmail.com>
2017-09-21 15:14:06 +0200

    Install xmobar as well

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ set -o errexit -o nounset -o pipefail
     cd "$(dirname "$0")"
 
     stack install --local-bin-path="${HOME}/.local/bin/"
+    stack install --local-bin-path="${HOME}/.local/bin/" xmobar
 )

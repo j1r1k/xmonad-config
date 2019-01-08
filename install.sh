f13bb9c8d3ce8f9ab9f9f2e7a8738beaa63e0dcb
Jiri Marsicek <jiri.marsicek@gmail.com>
2019-01-08 10:46:03 +0100

    mkfifo in install script

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 set -o errexit -o nounset -o pipefail
 
+mkpipe() {
+    PIPE="${HOME}/.xmonad/xmobar-pipe-${1}"
+    if [ ! -p "${PIPE}" ]; then
+        rm -f "$PIPE"
+        mkfifo "$PIPE"
+    fi
+}
+
+mkpipe bluetooth
+mkpipe audio
+
 (
     cd "$(dirname "$0")"
 
